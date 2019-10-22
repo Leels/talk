5e07f4ea79e2d050cecd2421fe726312ea85a101
Student <example@example.com>
2019-10-22 16:49:22 -0700

    Add JS functions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,22 @@
 $(document).ready(function() {
   $("button#hello").click(function() {
-    $("ul").prepend("<li>Hello!</li>");
-  });
+    $("ul#user").prepend("<li>Meow?</li>");
+    $("ul#webpage").prepend("<li>Bow-wow!</li>");
+    $("ul#user").children("li").first().click(function() {
+  $(this).remove();
+});
+$("ul#webpage").children("li").first().click(function() {
+  $(this).remove();
+});
+});
 
   $("button#goodbye").click(function() {
-    $("ul").prepend("<li>Goodbye!</li>");
+    $("ul#user").prepend("<li>Hiiissssssssss</li>");
+    $("ul#webpage").prepend("<li>Whimper whimper</li>");
   });
 
   $("button#stop").click(function() {
-    $("ul").prepend("<li>Stop copying me!</li>");
+    $("ul#user").prepend("<li>Puurrrrrr</li>");
+    $("ul#webpage").prepend("<li>Silence</li>");
   });
 });
